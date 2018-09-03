e7ee06c29fae2dea51bb2af335b115bac638043d
root <root@shuai-gcp-taiwan-server.c.delta-suprstate-189512.internal>
2018-09-03 14:08:59 +0000

    now templates can read the css version1.0.2

--- djan/settings.py
+++ djan/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i8dzcs-t18ra8%(fpu*#%wi27(#xpd@q#mtokoav-=tg*yri0$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.189.167.4','smart-brain.top','www.smart-brain.top']
 
 
 # Application definition
@@ -117,4 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+            os.path.join(BASE_DIR, "static"),
+                '/var/www/djan/static/',
+                )
