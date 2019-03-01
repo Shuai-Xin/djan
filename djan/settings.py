fff355a8c1c5bc738c778b00336418c7f509407b
Shuai <xinver22@gmail.com>
2019-03-01 06:59:44 +0000

    move away from root to user home

--- djan/settings.py
+++ djan/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
             os.path.join(BASE_DIR, "static"),
-                '/var/www/djan/static/',
+                '/home/xinver22/djan/static/',
                 
             )
